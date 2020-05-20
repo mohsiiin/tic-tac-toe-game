5fbca2d52e54d0e4ffe59218dd73591cf42499a1
mohsin khan <60709518+mohsiiin@users.noreply.github.com>
2020-05-20 19:03:09 +0530

    Update tik tac toe game.py

--- tik tac toe game.py
+++ tik tac toe game.py
@@ -2,15 +2,12 @@ print('\n'*100)
 
 def display_board(board):
     
-    print(board[7]+'|'+board[8]+'|'+board[9])
-    print('-----')
-    print(board[4]+'|'+board[5]+'|'+board[6])
-    print('-----')
-    print(board[1]+'|'+board[2]+'|'+board[3])
+    print(board[7]+' | '+board[8]+' | '+board[9])
+    print('---------')
+    print(board[4]+' | '+board[5]+' | '+board[6])
+    print('---------')
+    print(board[1]+' | '+board[2]+' | '+board[3])
 
-
-test_board = ['#','X','O','X','O','X','O','X','O','X']
-display_board(test_board)
 
 def player_input():
     
